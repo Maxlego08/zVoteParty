a2b87405c6d0fa82f5e7b606d4a8da9bca5dadab
Maxlego08 <maxlego08@sfr.fr>
2021-12-12 18:24:28 +0100

    :construction: Update to 1.18

--- src/fr/maxlego08/zvoteparty/command/commands/CommandVersion.java
+++ src/fr/maxlego08/zvoteparty/command/commands/CommandVersion.java
@@ -21,6 +21,7 @@ public class CommandVersion extends VCommand {
 		message(sender, "§aDiscord§7: §2http://discord.groupez.dev/");
 		message(sender, "§aDownload now§7: §2https://groupez.dev/resources/124");
 		message(sender, "§aServeur Minecraft Vote§7: §fhttps://serveur-minecraft-vote.fr/");
+		message(sender, "§aSponsor§7: §chttps://serveur-minecraft-vote.fr/?ref=345");
 		
 		return CommandType.SUCCESS;
 	}
