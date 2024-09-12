35950805ae053c203853a044018214dccc7516f0
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:19:41 +0500

    Update CommandRemove.java

--- src/main/java/fr/maxlego08/zvoteparty/command/commands/CommandRemove.java
+++ src/main/java/fr/maxlego08/zvoteparty/command/commands/CommandRemove.java
@@ -10,21 +10,18 @@ import fr.maxlego08.zvoteparty.zcore.utils.commands.CommandType;
 
 public class CommandRemove extends VCommand {
 
-	public CommandRemove(ZVotePartyPlugin plugin) {
-		super(plugin);
-		this.setDescription(Message.DESCRIPTION_REMOVE);
-		this.addSubCommand("remove");
-		this.setPermission(Permission.ZVOTEPARTY_REMOVE);
-		this.addRequireArg("player");
-	}
+    public CommandRemove(ZVotePartyPlugin plugin) {
+        super(plugin);
+        setDescription(Message.DESCRIPTION_REMOVE);
+        addSubCommand("remove");
+        setPermission(Permission.ZVOTEPARTY_REMOVE);
+        addRequireArg("player");
+    }
 
-	@Override
-	protected CommandType perform(ZVotePartyPlugin plugin) {
-		
-		OfflinePlayer player = this.argAsOfflinePlayer(0);
-		this.plugin.getManager().removeVote(this.sender, player);
-		
-		return CommandType.SUCCESS;
-	}
-
+    @Override
+    protected CommandType perform(ZVotePartyPlugin plugin) {
+        OfflinePlayer player = argAsOfflinePlayer(0);
+        plugin.getManager().removeVote(sender, player);
+        return CommandType.SUCCESS;
+    }
 }
