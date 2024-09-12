dc65c1278f1498cb0e5718873b3c19983b043f86
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:30:56 +0500

    Update ListenerNullException.java

--- src/main/java/fr/maxlego08/zvoteparty/exceptions/ListenerNullException.java
+++ src/main/java/fr/maxlego08/zvoteparty/exceptions/ListenerNullException.java
@@ -1,35 +1,56 @@
 package fr.maxlego08.zvoteparty.exceptions;
 
+/**
+ * Exception thrown when a listener is null.
+ */
 public class ListenerNullException extends Error {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public ListenerNullException() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Default constructor.
+     */
+    public ListenerNullException() {
+        super(); // Calls the default constructor of Error
+    }
 
-	public ListenerNullException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message.
+     * 
+     * @param message The detail message for the exception.
+     */
+    public ListenerNullException(String message) {
+        super(message); // Passes the message to Error constructor
+    }
 
-	public ListenerNullException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause The cause of the exception.
+     */
+    public ListenerNullException(Throwable cause) {
+        super(cause); // Passes the cause to Error constructor
+    }
 
-	public ListenerNullException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message and a cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     */
+    public ListenerNullException(String message, Throwable cause) {
+        super(message, cause); // Passes both message and cause to Error constructor
+    }
 
-	public ListenerNullException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
-
+    /**
+     * Constructor with a message, cause, suppression flag, and writable stack trace flag.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     * @param enableSuppression Whether or not suppression is enabled.
+     * @param writableStackTrace Whether or not the stack trace is writable.
+     */
+    public ListenerNullException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace); // Passes all parameters to Error constructor
+    }
 }
