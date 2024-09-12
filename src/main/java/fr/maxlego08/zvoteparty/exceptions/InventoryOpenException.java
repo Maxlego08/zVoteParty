8752fcb797a9d0160b0f32fa7548689867b3f6d6
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:27:35 +0500

    Update InventoryOpenException.java

--- src/main/java/fr/maxlego08/zvoteparty/exceptions/InventoryOpenException.java
+++ src/main/java/fr/maxlego08/zvoteparty/exceptions/InventoryOpenException.java
@@ -1,35 +1,56 @@
 package fr.maxlego08.zvoteparty.exceptions;
 
+/**
+ * Exception thrown when there is an issue opening an inventory.
+ */
 public class InventoryOpenException extends Exception {
 
-	
+    private static final long serialVersionUID = 1L;
 
-	public InventoryOpenException() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Default constructor.
+     */
+    public InventoryOpenException() {
+        super(); // Calls the default constructor of Exception
+    }
 
-	public InventoryOpenException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message and a cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     * @param enableSuppression Whether or not suppression is enabled.
+     * @param writableStackTrace Whether or not the stack trace is writable.
+     */
+    public InventoryOpenException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace); // Passes all parameters to the Exception constructor
+    }
 
-	public InventoryOpenException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message and a cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     */
+    public InventoryOpenException(String message, Throwable cause) {
+        super(message, cause); // Passes both message and cause to the Exception constructor
+    }
 
-	public InventoryOpenException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message.
+     * 
+     * @param message The detail message for the exception.
+     */
+    public InventoryOpenException(String message) {
+        super(message); // Passes the message to the Exception constructor
+    }
 
-	public InventoryOpenException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	private static final long serialVersionUID = 1L;
-
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause The cause of the exception.
+     */
+    public InventoryOpenException(Throwable cause) {
+        super(cause); // Passes the cause to the Exception constructor
+    }
 }
