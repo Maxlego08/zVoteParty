3da1603e14ccfe6ac6418625d8ec47874310e565
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:28:39 +0500

    Update InventoryTypeException.java

--- src/main/java/fr/maxlego08/zvoteparty/exceptions/InventoryTypeException.java
+++ src/main/java/fr/maxlego08/zvoteparty/exceptions/InventoryTypeException.java
@@ -1,55 +1,56 @@
 package fr.maxlego08.zvoteparty.exceptions;
 
+/**
+ * Exception thrown when there is an issue with the inventory type.
+ */
 public class InventoryTypeException extends InventoryOpenException {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 
-	 */
-	public InventoryTypeException() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Default constructor.
+     */
+    public InventoryTypeException() {
+        super(); // Calls the default constructor of InventoryOpenException
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public InventoryTypeException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message.
+     * 
+     * @param message The detail message for the exception.
+     */
+    public InventoryTypeException(String message) {
+        super(message); // Passes the message to InventoryOpenException constructor
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 */
-	public InventoryTypeException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause The cause of the exception.
+     */
+    public InventoryTypeException(Throwable cause) {
+        super(cause); // Passes the cause to InventoryOpenException constructor
+    }
 
-	/**
-	 * @param message
-	 */
-	public InventoryTypeException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message and a cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     */
+    public InventoryTypeException(String message, Throwable cause) {
+        super(message, cause); // Passes both message and cause to InventoryOpenException constructor
+    }
 
-	/**
-	 * @param cause
-	 */
-	public InventoryTypeException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
-
+    /**
+     * Constructor with a message, cause, suppression flag, and writable stack trace flag.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     * @param enableSuppression Whether or not suppression is enabled.
+     * @param writableStackTrace Whether or not the stack trace is writable.
+     */
+    public InventoryTypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace); // Passes all parameters to InventoryOpenException constructor
+    }
 }
