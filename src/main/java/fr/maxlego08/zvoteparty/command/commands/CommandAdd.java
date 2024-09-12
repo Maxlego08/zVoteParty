05e9b30747a700671b89bbcbf6c2c94806713e82
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:09:26 +0500

    Optimise CommandAdd.java

--- src/main/java/fr/maxlego08/zvoteparty/command/commands/CommandAdd.java
+++ src/main/java/fr/maxlego08/zvoteparty/command/commands/CommandAdd.java
@@ -8,23 +8,20 @@ import fr.maxlego08.zvoteparty.zcore.utils.commands.CommandType;
 
 public class CommandAdd extends VCommand {
 
-	public CommandAdd(ZVotePartyPlugin plugin) {
-		super(plugin);
-		this.setDescription(Message.DESCRIPTION_ADD);
-		this.addSubCommand("add");
-		this.setPermission(Permission.ZVOTEPARTY_ADD);
-		this.addRequireArg("player");
-		this.addOptionalArg("party (true/false)");
-	}
+    public CommandAdd(ZVotePartyPlugin plugin) {
+        super(plugin);
+        setDescription(Message.DESCRIPTION_ADD);
+        addSubCommand("add");
+        setPermission(Permission.ZVOTEPARTY_ADD);
+        addRequireArg("player");
+        addOptionalArg("party (true/false)");
+    }
 
-	@Override
-	protected CommandType perform(ZVotePartyPlugin plugin) {
-		
-		String player = this.argAsString(0);
-		boolean updateVoteParty = this.argAsBoolean(1, false);
-		this.plugin.getManager().vote(this.sender, player, updateVoteParty);
-		
-		return CommandType.SUCCESS;
-	}
-
+    @Override
+    protected CommandType perform(ZVotePartyPlugin plugin) {
+        String player = argAsString(0);
+        boolean updateVoteParty = argAsBoolean(1, false);
+        plugin.getManager().vote(sender, player, updateVoteParty);
+        return CommandType.SUCCESS;
+    }
 }
