0e025099de01e1e8c51f3bbf44bda741733e0140
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:23:00 +0500

    Update CommandVote.java

--- src/main/java/fr/maxlego08/zvoteparty/command/commands/CommandVote.java
+++ src/main/java/fr/maxlego08/zvoteparty/command/commands/CommandVote.java
@@ -8,19 +8,16 @@ import fr.maxlego08.zvoteparty.zcore.utils.commands.CommandType;
 
 public class CommandVote extends VCommand {
 
-	public CommandVote(ZVotePartyPlugin plugin) {
-		super(plugin);
-		this.setDescription(Message.DESCRIPTION_HELP);
-		this.setPermission(Permission.ZVOTEPARTY_VOTE);
-		this.setConsoleCanUse(false);
-	}
+    public CommandVote(ZVotePartyPlugin plugin) {
+        super(plugin);
+        setDescription(Message.DESCRIPTION_VOTE); // Adjusted to correct message description
+        setPermission(Permission.ZVOTEPARTY_VOTE);
+        setConsoleCanUse(false);
+    }
 
-	@Override
-	protected CommandType perform(ZVotePartyPlugin plugin) {
-
-		this.manager.openVote(this.player);
-		
-		return CommandType.SUCCESS;
-	}
-
+    @Override
+    protected CommandType perform(ZVotePartyPlugin plugin) {
+        manager.openVote(player);
+        return CommandType.SUCCESS;
+    }
 }
