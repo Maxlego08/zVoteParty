f80ef8bea2f0b427aa946fd73ac823850f99156d
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:35:08 +0500

    Update ZVote.java

--- src/main/java/fr/maxlego08/zvoteparty/implementations/ZVote.java
+++ src/main/java/fr/maxlego08/zvoteparty/implementations/ZVote.java
@@ -6,58 +6,67 @@ import org.bukkit.plugin.Plugin;
 import fr.maxlego08.zvoteparty.api.Reward;
 import fr.maxlego08.zvoteparty.api.Vote;
 
+/**
+ * Represents a vote with its associated service, creation time, reward, and reward status.
+ */
 public class ZVote implements Vote {
 
-	private final String serviceName;
-	private final long createdAt;
-	private final Reward reward;
-	private boolean rewardIsGive;
+    private final String serviceName;
+    private final long createdAt;
+    private final Reward reward;
+    private boolean rewardIsGiven;
 
-	/**
-	 * @param serviceName
-	 * @param createdAt
-	 * @param reward
-	 * @param rewardIsGive
-	 */
-	public ZVote(String serviceName, long createdAt, Reward reward, boolean rewardIsGive) {
-		super();
-		this.serviceName = serviceName;
-		this.createdAt = createdAt;
-		this.reward = reward;
-		this.rewardIsGive = rewardIsGive;
-	}
+    /**
+     * Constructs a ZVote with the specified parameters.
+     *
+     * @param serviceName the name of the service related to the vote
+     * @param createdAt the timestamp when the vote was created
+     * @param reward the reward associated with the vote
+     * @param rewardIsGiven whether the reward has been given
+     */
+    public ZVote(String serviceName, long createdAt, Reward reward, boolean rewardIsGiven) {
+        this.serviceName = serviceName;
+        this.createdAt = createdAt;
+        this.reward = reward;
+        this.rewardIsGiven = rewardIsGiven;
+    }
 
-	/**
-	 * @param link
-	 */
-	public ZVote(String serviceName, Reward reward, boolean rewardIsGive) {
-		this(serviceName, System.currentTimeMillis(), reward, rewardIsGive);
-	}
+    /**
+     * Constructs a ZVote with the current timestamp.
+     *
+     * @param serviceName the name of the service related to the vote
+     * @param reward the reward associated with the vote
+     * @param rewardIsGiven whether the reward has been given
+     */
+    public ZVote(String serviceName, Reward reward, boolean rewardIsGiven) {
+        this(serviceName, System.currentTimeMillis(), reward, rewardIsGiven);
+    }
 
-	@Override
-	public String getServiceName() {
-		return this.serviceName;
-	}
+    @Override
+    public String getServiceName() {
+        return serviceName;
+    }
 
-	@Override
-	public long getCreatedAt() {
-		return this.createdAt;
-	}
+    @Override
+    public long getCreatedAt() {
+        return createdAt;
+    }
 
-	@Override
-	public Reward getReward() {
-		return this.reward;
-	}
+    @Override
+    public Reward getReward() {
+        return reward;
+    }
 
-	@Override
-	public boolean rewardIsGive() {
-		return this.rewardIsGive;
-	}
+    @Override
+    public boolean rewardIsGiven() {
+        return rewardIsGiven;
+    }
 
-	@Override
-	public void giveReward(Plugin plugin, Player player) {
-		this.rewardIsGive = true;
-		this.reward.give(plugin, player);
-	}
-
+    @Override
+    public void giveReward(Plugin plugin, Player player) {
+        if (!rewardIsGiven) {
+            rewardIsGiven = true;
+            reward.give(plugin, player);
+        }
+    }
 }
