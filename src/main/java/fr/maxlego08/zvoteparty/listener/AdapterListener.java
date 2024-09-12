d810288fc81df1f7bed062a256717f92d7e7c50a
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:38:30 +0500

    Update AdapterListener.java

--- src/main/java/fr/maxlego08/zvoteparty/listener/AdapterListener.java
+++ src/main/java/fr/maxlego08/zvoteparty/listener/AdapterListener.java
@@ -14,36 +14,75 @@ import fr.maxlego08.zvoteparty.zcore.utils.ZUtils;
 
 public class AdapterListener extends ZUtils implements Listener {
 
-	private final ZVotePartyPlugin template;
+    private final ZVotePartyPlugin plugin;
 
-	public AdapterListener(ZVotePartyPlugin template) {
-		this.template = template;
-	}
+    /**
+     * Constructor to initialize the AdapterListener with the plugin instance.
+     * 
+     * @param plugin The instance of ZVotePartyPlugin
+     */
+    public AdapterListener(ZVotePartyPlugin plugin) {
+        this.plugin = plugin;
+    }
 
-	@EventHandler
-	public void onConnect(PlayerJoinEvent event) {
-		template.getListenerAdapters().forEach(adapter -> adapter.onConnect(event, event.getPlayer()));
-	}
+    /**
+     * Handles player join events by notifying all registered listener adapters.
+     * 
+     * @param event The PlayerJoinEvent
+     */
+    @EventHandler
+    public void onConnect(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        plugin.getListenerAdapters().forEach(adapter -> adapter.onConnect(event, player));
+    }
 
-	@EventHandler
-	public void onQuit(PlayerQuitEvent event) {
-		template.getListenerAdapters().forEach(adapter -> adapter.onQuit(event, event.getPlayer()));
-	}
-	
-	@EventHandler
-	public void onInventoryClick(InventoryClickEvent event) {
-		template.getListenerAdapters()
-				.forEach(adapter -> adapter.onInventoryClick(event, (Player) event.getWhoClicked()));
-	}
+    /**
+     * Handles player quit events by notifying all registered listener adapters.
+     * 
+     * @param event The PlayerQuitEvent
+     */
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        plugin.getListenerAdapters().forEach(adapter -> adapter.onQuit(event, player));
+    }
 
-	@EventHandler
-	public void onDrag(InventoryDragEvent event) {
-		template.getListenerAdapters()
-				.forEach(adapter -> adapter.onInventoryDrag(event, (Player) event.getWhoClicked()));
-	}
+    /**
+     * Handles inventory click events by notifying all registered listener adapters.
+     * 
+     * @param event The InventoryClickEvent
+     */
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getWhoClicked() instanceof Player) {
+            Player player = (Player) event.getWhoClicked();
+            plugin.getListenerAdapters().forEach(adapter -> adapter.onInventoryClick(event, player));
+        }
+    }
 
-	@EventHandler
-	public void onClose(InventoryCloseEvent event) {
-		template.getListenerAdapters().forEach(adapter -> adapter.onInventoryClose(event, (Player) event.getPlayer()));
-	}
+    /**
+     * Handles inventory drag events by notifying all registered listener adapters.
+     * 
+     * @param event The InventoryDragEvent
+     */
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent event) {
+        if (event.getWhoClicked() instanceof Player) {
+            Player player = (Player) event.getWhoClicked();
+            plugin.getListenerAdapters().forEach(adapter -> adapter.onInventoryDrag(event, player));
+        }
+    }
+
+    /**
+     * Handles inventory close events by notifying all registered listener adapters.
+     * 
+     * @param event The InventoryCloseEvent
+     */
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        if (event.getPlayer() instanceof Player) {
+            Player player = (Player) event.getPlayer();
+            plugin.getListenerAdapters().forEach(adapter -> adapter.onInventoryClose(event, player));
+        }
+    }
 }
