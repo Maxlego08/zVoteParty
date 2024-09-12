53f538f809d063698a14e1d15540ebe162ea11c8
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:26:18 +0500

    Update InventoryAlreadyExistException.java

--- src/main/java/fr/maxlego08/zvoteparty/exceptions/InventoryAlreadyExistException.java
+++ src/main/java/fr/maxlego08/zvoteparty/exceptions/InventoryAlreadyExistException.java
@@ -1,36 +1,56 @@
 package fr.maxlego08.zvoteparty.exceptions;
 
+/**
+ * Exception thrown when an inventory already exists.
+ */
 public class InventoryAlreadyExistException extends Error {
 
-	public InventoryAlreadyExistException() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    private static final long serialVersionUID = -5611455794293458580L;
 
-	public InventoryAlreadyExistException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Default constructor.
+     */
+    public InventoryAlreadyExistException() {
+        super(); // Calls the default constructor of Error
+    }
 
-	public InventoryAlreadyExistException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message and a cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     */
+    public InventoryAlreadyExistException(String message, Throwable cause) {
+        super(message, cause); // Passes both message and cause to the Error constructor
+    }
 
-	public InventoryAlreadyExistException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message.
+     * 
+     * @param message The detail message for the exception.
+     */
+    public InventoryAlreadyExistException(String message) {
+        super(message); // Passes the message to the Error constructor
+    }
 
-	public InventoryAlreadyExistException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause The cause of the exception.
+     */
+    public InventoryAlreadyExistException(Throwable cause) {
+        super(cause); // Passes the cause to the Error constructor
+    }
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5611455794293458580L;
-
+    /**
+     * Constructor with a message, a cause, suppression, and writable stack trace.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     * @param enableSuppression Whether or not suppression is enabled.
+     * @param writableStackTrace Whether or not the stack trace is writable.
+     */
+    public InventoryAlreadyExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace); // Passes all parameters to the Error constructor
+    }
 }
