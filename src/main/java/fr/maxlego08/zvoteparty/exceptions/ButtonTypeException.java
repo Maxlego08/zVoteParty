e410ecda1917e4c891ac0963aaddaeab684d04b3
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:24:34 +0500

    Update ButtonTypeException.java

--- src/main/java/fr/maxlego08/zvoteparty/exceptions/ButtonTypeException.java
+++ src/main/java/fr/maxlego08/zvoteparty/exceptions/ButtonTypeException.java
@@ -1,60 +1,58 @@
-/**
- * 
- */
 package fr.maxlego08.zvoteparty.exceptions;
 
 /**
+ * Custom exception for button type errors.
+ * 
  * @author Maxlego08
- *
  */
 public class ButtonTypeException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 
-	 */
-	public ButtonTypeException() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Default constructor.
+     */
+    public ButtonTypeException() {
+        super(); // Calls the default constructor of Exception
+    }
 
-	/**
-	 * @param message
-	 */
-	public ButtonTypeException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message.
+     * 
+     * @param message The detail message for the exception.
+     */
+    public ButtonTypeException(String message) {
+        super(message); // Passes the message to the Exception constructor
+    }
 
-	/**
-	 * @param cause
-	 */
-	public ButtonTypeException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause The cause of the exception.
+     */
+    public ButtonTypeException(Throwable cause) {
+        super(cause); // Passes the cause to the Exception constructor
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 */
-	public ButtonTypeException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message and a cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     */
+    public ButtonTypeException(String message, Throwable cause) {
+        super(message, cause); // Passes both message and cause to the Exception constructor
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public ButtonTypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
-
+    /**
+     * Constructor with a message, a cause, suppression, and writable stack trace.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     * @param enableSuppression Whether or not suppression is enabled.
+     * @param writableStackTrace Whether or not the stack trace is writable.
+     */
+    public ButtonTypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace); // Passes all parameters to the Exception constructor
+    }
 }
