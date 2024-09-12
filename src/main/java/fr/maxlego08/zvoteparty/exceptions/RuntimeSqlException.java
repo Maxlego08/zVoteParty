4eebd2f55e76a83c5b50b4d1be4ab39264389f47
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:31:35 +0500

    Update RuntimeSqlException.java

--- src/main/java/fr/maxlego08/zvoteparty/exceptions/RuntimeSqlException.java
+++ src/main/java/fr/maxlego08/zvoteparty/exceptions/RuntimeSqlException.java
@@ -1,23 +1,44 @@
 package fr.maxlego08.zvoteparty.exceptions;
 
+/**
+ * Exception thrown for SQL runtime errors.
+ */
 public class RuntimeSqlException extends RuntimeException {
 
-	private static final long serialVersionUID = 5224696788505678598L;
+    private static final long serialVersionUID = 5224696788505678598L;
 
-	public RuntimeSqlException() {
-		super();
-	}
+    /**
+     * Default constructor.
+     */
+    public RuntimeSqlException() {
+        super(); // Calls the default constructor of RuntimeException
+    }
 
-	public RuntimeSqlException(String message) {
-		super(message);
-	}
+    /**
+     * Constructor with a message.
+     * 
+     * @param message The detail message for the exception.
+     */
+    public RuntimeSqlException(String message) {
+        super(message); // Passes the message to RuntimeException constructor
+    }
 
-	public RuntimeSqlException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    /**
+     * Constructor with a message and cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     */
+    public RuntimeSqlException(String message, Throwable cause) {
+        super(message, cause); // Passes both message and cause to RuntimeException constructor
+    }
 
-	public RuntimeSqlException(Throwable cause) {
-		super(cause);
-	}
-
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause The cause of the exception.
+     */
+    public RuntimeSqlException(Throwable cause) {
+        super(cause); // Passes the cause to RuntimeException constructor
+    }
 }
