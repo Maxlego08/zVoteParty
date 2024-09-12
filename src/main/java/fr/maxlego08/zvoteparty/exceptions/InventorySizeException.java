3bfee8d66e2696d991b79b7fc7f640ae4166dda7
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:28:15 +0500

    Update InventorySizeException.java

--- src/main/java/fr/maxlego08/zvoteparty/exceptions/InventorySizeException.java
+++ src/main/java/fr/maxlego08/zvoteparty/exceptions/InventorySizeException.java
@@ -1,61 +1,56 @@
-/**
- * 
- */
 package fr.maxlego08.zvoteparty.exceptions;
 
 /**
- * @author Maxlego08
- *
+ * Exception thrown when there is an issue with the inventory size.
  */
 public class InventorySizeException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 
-	 */
-	public InventorySizeException() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Default constructor.
+     */
+    public InventorySizeException() {
+        super(); // Calls the default constructor of Exception
+    }
 
-	/**
-	 * @param message
-	 */
-	public InventorySizeException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message.
+     * 
+     * @param message The detail message for the exception.
+     */
+    public InventorySizeException(String message) {
+        super(message); // Passes the message to the Exception constructor
+    }
 
-	/**
-	 * @param cause
-	 */
-	public InventorySizeException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause The cause of the exception.
+     */
+    public InventorySizeException(Throwable cause) {
+        super(cause); // Passes the cause to the Exception constructor
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 */
-	public InventorySizeException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message and a cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     */
+    public InventorySizeException(String message, Throwable cause) {
+        super(message, cause); // Passes both message and cause to the Exception constructor
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public InventorySizeException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
-
+    /**
+     * Constructor with a message, cause, suppression flag, and writable stack trace flag.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     * @param enableSuppression Whether or not suppression is enabled.
+     * @param writableStackTrace Whether or not the stack trace is writable.
+     */
+    public InventorySizeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace); // Passes all parameters to the Exception constructor
+    }
 }
