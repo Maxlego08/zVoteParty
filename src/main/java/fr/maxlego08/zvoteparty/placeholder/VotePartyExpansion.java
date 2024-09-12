7f70459170db753d274d32c10aa9621bdb27019f
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:36:43 +0500

    Update VotePartyExpansion.java

--- src/main/java/fr/maxlego08/zvoteparty/placeholder/VotePartyExpansion.java
+++ src/main/java/fr/maxlego08/zvoteparty/placeholder/VotePartyExpansion.java
@@ -7,39 +7,41 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
 public class VotePartyExpansion extends PlaceholderExpansion {
 
-	private final Plugin plugin;
+    private final Plugin plugin;
 
-	/**
-	 * @param plugin
-	 */
-	public VotePartyExpansion(Plugin plugin) {
-		super();
-		this.plugin = plugin;
-	}
+    /**
+     * Constructs a new VotePartyExpansion instance.
+     * 
+     * @param plugin the plugin instance
+     */
+    public VotePartyExpansion(Plugin plugin) {
+        this.plugin = plugin;
+    }
 
-	@Override
-	public String getAuthor() {
-		return "Maxlego08";
-	}
+    @Override
+    public String getAuthor() {
+        return "Maxlego08";
+    }
 
-	@Override
-	public String getIdentifier() {
-		return "zvoteparty";
-	}
+    @Override
+    public String getIdentifier() {
+        return "zvoteparty";
+    }
 
-	@Override
-	public String getVersion() {
-		return plugin.getDescription().getVersion();
-	}
+    @Override
+    public String getVersion() {
+        return plugin.getDescription().getVersion();
+    }
 
-	@Override
-	public String onPlaceholderRequest(Player player, String params) {
-		return ZPlaceholderApi.getInstance().onRequest(player, params);
-	}
+    @Override
+    public String onPlaceholderRequest(Player player, String params) {
+        // Delegate placeholder request handling to ZPlaceholderApi
+        return ZPlaceholderApi.getInstance().onRequest(player, params);
+    }
 
-	@Override
-	public boolean persist() {
-		return true;
-	}
-	
+    @Override
+    public boolean persist() {
+        // Indicates that this placeholder expansion should persist
+        return true;
+    }
 }
