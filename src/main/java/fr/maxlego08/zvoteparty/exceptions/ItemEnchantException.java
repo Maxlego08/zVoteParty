bcf7d5f0e769ee6b8f0e3073ced19fc9e6e513a1
Abasing <140362885+Abasing@users.noreply.github.com>
2024-09-13 00:29:58 +0500

    Update ItemEnchantException.java

--- src/main/java/fr/maxlego08/zvoteparty/exceptions/ItemEnchantException.java
+++ src/main/java/fr/maxlego08/zvoteparty/exceptions/ItemEnchantException.java
@@ -1,61 +1,56 @@
-/**
- * 
- */
 package fr.maxlego08.zvoteparty.exceptions;
 
 /**
- * @author Maxlego08
- *
+ * Exception thrown when there is an issue with item enchanting.
  */
 public class ItemEnchantException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 
-	 */
-	public ItemEnchantException() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Default constructor.
+     */
+    public ItemEnchantException() {
+        super(); // Calls the default constructor of Exception
+    }
 
-	/**
-	 * @param message
-	 */
-	public ItemEnchantException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message.
+     * 
+     * @param message The detail message for the exception.
+     */
+    public ItemEnchantException(String message) {
+        super(message); // Passes the message to Exception constructor
+    }
 
-	/**
-	 * @param cause
-	 */
-	public ItemEnchantException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a cause.
+     * 
+     * @param cause The cause of the exception.
+     */
+    public ItemEnchantException(Throwable cause) {
+        super(cause); // Passes the cause to Exception constructor
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 */
-	public ItemEnchantException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor with a message and a cause.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     */
+    public ItemEnchantException(String message, Throwable cause) {
+        super(message, cause); // Passes both message and cause to Exception constructor
+    }
 
-	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public ItemEnchantException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
-
+    /**
+     * Constructor with a message, cause, suppression flag, and writable stack trace flag.
+     * 
+     * @param message The detail message for the exception.
+     * @param cause The cause of the exception.
+     * @param enableSuppression Whether or not suppression is enabled.
+     * @param writableStackTrace Whether or not the stack trace is writable.
+     */
+    public ItemEnchantException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace); // Passes all parameters to Exception constructor
+    }
 }
